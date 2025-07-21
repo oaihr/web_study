db399d7fd3f85784241653109c803c75fee76a96
oaihr <rhksgh0710@gmail.com>
2025-07-21 12:22:47 +0900

    객실 신규 등록, 유저 서비스 구조 추가

--- src/main/java/com/app/service/room/impl/RoomServiceImpl.java
+++ src/main/java/com/app/service/room/impl/RoomServiceImpl.java
@@ -44,6 +44,14 @@ public class RoomServiceImpl implements RoomService{
 	}
 
 
+	@Override
+	public int saveRoom(Room room) {
+		// room 정보 저장해라~ -> DAO
+		int result = roomDAO.saveRoom(room);
+		return result;
+	}
+
+
 	
 
 }
