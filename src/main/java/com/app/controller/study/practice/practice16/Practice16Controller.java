f892720911350ae59e81d71cd4fa1ee026db9cc4
oaihr <rhksgh0710@gmail.com>
2025-07-25 14:29:32 +0900

    연습16 REST API 응답 처리

--- src/main/java/com/app/controller/study/practice/practice16/Practice16Controller.java
+++ src/main/java/com/app/controller/study/practice/practice16/Practice16Controller.java
@@ -1,42 +1,35 @@
 package com.app.controller.study.practice.practice16;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.app.common.CommonCode;
 import com.app.dto.user.User;
-import com.app.service.user.UserService;
 
 @Controller
 public class Practice16Controller {
 	
-//	@Autowired
-//	UserService userService;
-
-//	MainController로 옮김
+	@GetMapping("/practice16/quiz1")
+	public String quiz1() {
+		return "practice/practice16/quiz1";
+	}
 	
-//	@GetMapping("/")
-//	public String main() {
-//		return "practice/practice16/main";
-//	}
+	@ResponseBody
+	@GetMapping("/practice16/quiz2")
+	public String quiz2() {
+		return "return text quiz2";
+	}
 	
-	
-//	CustomerController로 옮김
-//	@GetMapping("/customer/signup")
-//	public String signup() {		
-//		return "practice/practice16/signup";
-//	}
-//	
-//	@PostMapping("/customer/signup")
-//	public String signupAction(User user) {
-//		int result = userService.saveCustomerUser(user);
-//		if(result>0) {
-//			System.out.println("회원가입 완료");
-//			return "redirect:/admin/users";
-//		}else {
-//			System.out.println("회원가입 실패");
-//			return "practice/practice16/signup";
-//		}		
-//	}
+	@ResponseBody
+	@GetMapping("/practice16/quiz3")
+	public User quiz3() {
+		User user = new User();
+		user.setId("quizid");
+		user.setPw("secret");
+		user.setName("quiz이름");
+		user.setUserType(CommonCode.USER_USERTYPE_CUSTOMER);
+		
+		return user;
+	}
 }
