d07a4609c80b5f6924b33b434a45174b5792c4eb
oaihr <rhksgh0710@gmail.com>
2025-07-31 10:42:05 +0900

    유효성 검증 2 (DTO 어노테이션 기준)

--- src/main/java/com/app/controller/customer/CustomerController.java
+++ src/main/java/com/app/controller/customer/CustomerController.java
@@ -1,11 +1,17 @@
 package com.app.controller.customer;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,7 +42,24 @@ public class CustomerController {
 	}
 	
 	@PostMapping("/customer/signup")
-	public String signupAction(User user) {
+	public String signupAction(@Valid @ModelAttribute User user, BindingResult br) {
+								//유효성검사			검사 결과
+		
+		//검증 결과에 문제가 있냐 없냐
+		if(br.hasErrors()) { //문제O
+			//문제 내용을 출력
+			List<ObjectError> errorList = br.getAllErrors();
+			for(ObjectError er : errorList) {
+				System.out.println(er.getObjectName());
+				System.out.println(er.getDefaultMessage());
+				System.out.println(er.getCode());
+				System.out.println(er.getCodes()[0]);
+			}
+			return "customer/signup";
+		}
+		
+		
+		
 		int result = userService.saveCustomerUser(user);
 		if(result>0) {
 			System.out.println("회원가입 완료");
--- src/main/java/com/app/dto/user/User.java
+++ src/main/java/com/app/dto/user/User.java
@@ -1,11 +1,19 @@
 package com.app.dto.user;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class User {
 	
+	//필수입력 빈칸X 	뷰에서 ${errors.getFieldError('id').defaultMessage}
+	@NotBlank(message = "아이디 필수얌")
 	String id;
+	
+	//입력 길이 제한			뷰에서 ${errors.getFieldError('pw').defaultMessage}
+	@Size(min=8, max=12, message = "비밀번호 길이 체크해")
 	String pw;
 	String name;
 	String userType; // CUS / ADM
