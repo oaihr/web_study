2707828c495a30e32332479a5f2b5714bc698684
oaihr <rhksgh0710@gmail.com>
2025-08-21 15:25:57 +0900

    [React Spring] react 요청 통신용 REST API 추가

---
+++ src/main/java/com/app/dto/study/DrinkItem.java
@@ -0,0 +1,13 @@
+package com.app.dto.study;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DrinkItem {
+	String name;
+	String type;
+}
